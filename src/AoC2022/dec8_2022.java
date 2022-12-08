bae727d9b48bb5594f08c3691e96b68e125e60a6
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-12-08 08:16:30 +0100

    Aoc2022 day8 started

---
+++ src/AoC2022/dec8_2022.java
@@ -0,0 +1,26 @@
+package AoC2022;
+
+import java.io.FileNotFoundException;
+
+public class dec8_2022 {
+    public static void main(String[] args) throws FileNotFoundException {
+        int total = 0;
+        String in = "";
+        int height = 0;
+        String row = "";
+
+        in = dec2_2022.altReadFile("input/dec8.txt");
+
+        for (int i = 0; i < in.length(); i++) {
+
+
+            if (i < 100) {
+                total++;
+            } else if (in.charAt(i-1) == ' ' || in.charAt(i+1) == ' ') {
+                total++;
+            } else if (in.charAt(i) <= in.charAt(i-99)) { //Checks top but it doesnt always need to
+
+            }
+        }
+    }
+}
