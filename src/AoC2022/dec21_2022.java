56e308b1bc9b96d9ea21de04a935ca7d37539ef6
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-12-21 08:20:06 +0100

    Aoc2022 day21 started

---
+++ src/AoC2022/dec21_2022.java
@@ -0,0 +1,46 @@
+package AoC2022;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class dec21_2022 {
+    private String name;
+    private int num;
+
+    public dec21_2022(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("input/dec21.txt"));
+        String in;
+        dec21_2022[] monkey = new dec21_2022[2100];
+        int i = 0;
+
+        while (scan.hasNext()) {
+            in = scan.nextLine();
+            monkey[i] = new dec21_2022((in.charAt(0) + "" + in.charAt(1) + in.charAt(2) + in.charAt(3)), Integer.parseInt(String.valueOf(in.split(": "))));
+            i++;
+        }
+
+
+    }
+}
