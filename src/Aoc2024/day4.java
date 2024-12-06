0504d6895bacc919654d926916995d83276c7981
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-12-06 20:02:11 +0100

    Day 4 part 1 2024 fortsatt

--- src/Aoc2024/day4.java
+++ src/Aoc2024/day4.java
@@ -12,30 +12,32 @@ public class day4 {
 
         // Glöm inte att kolla kanter / Edgecases.
         for (int i = 0; i < rows.length; i++) {
-            // Vanlig
-            if (rows[i].charAt(i) == 'X' && rows[i].charAt(i+1) == 'M' && rows[i].charAt(i+2) == 'A' && rows[i].charAt(i+3) == 'S') {
-                times++;
-                // Bakvänt
-            } else if (rows[i].charAt(i) == 'S' && rows[i].charAt(i+1) == 'A' && rows[i].charAt(i+2) == 'M' && rows[i].charAt(i+3) == 'X') {
-                times++;
-                // Vertikalt
-            } else if (rows[i].charAt(i) == 'X' && rows[i+1].charAt(i) == 'M' && rows[i+2].charAt(i) == 'A' && rows[i].charAt(i+3) == 'S') {
-                times++;
-                // Vertikalt bakvänt
-            } else if (rows[i].charAt(i) == 'S' && rows[i+1].charAt(i) == 'A' && rows[i+2].charAt(i) == 'M' && rows[i].charAt(i+3) == 'X') {
-                times++;
-                // Diagonalt höger
-            } else if (rows[i].charAt(i) == 'X' && rows[i+1].charAt(i+1) == 'M' && rows[i+2].charAt(i+2) == 'A' && rows[i+3].charAt(i+3) == 'S') {
-                times++;
-                // Diagonalt höger bakvänt
-            } else if (rows[i].charAt(i) == 'S' && rows[i+1].charAt(i+1) == 'A' && rows[i+2].charAt(i+2) == 'M' && rows[i+3].charAt(i+3) == 'X') {
-                times++;
-                // Diagonalt vänster
-            } else if (rows[i].charAt(i) == 'X' && rows[i+1].charAt(i-1) == 'M' && rows[i+2].charAt(i-2) == 'A' && rows[i-3].charAt(i+3) == 'S') {
-                times++;
-                // Diagonalt vänster bakvänt
-            } else if (rows[i].charAt(i) == 'S' && rows[i+1].charAt(i-1) == 'A' && rows[i+2].charAt(i-2) == 'M' && rows[i-3].charAt(i+3) == 'X') {
-                times++;
+            for (int j = 0; j < rows[i].length(); j++) {
+                // Vanlig
+                if (rows[i].charAt(j) == 'X' && rows[i].charAt(j + 1) == 'M' && rows[i].charAt(j + 2) == 'A' && rows[i].charAt(j + 3) == 'S') {
+                    times++;
+                    // Bakvänt
+                } else if (rows[i].charAt(j) == 'S' && rows[i].charAt(j + 1) == 'A' && rows[i].charAt(j + 2) == 'M' && rows[i].charAt(j + 3) == 'X') {
+                    times++;
+                    // Vertikalt
+                } else if (rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j) == 'M' && rows[i + 2].charAt(j) == 'A' && rows[i+3].charAt(j) == 'S') {
+                    times++;
+                    // Vertikalt bakvänt
+                } else if (rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j) == 'A' && rows[i + 2].charAt(j) == 'M' && rows[i+3].charAt(j) == 'X') {
+                    times++;
+                    // Diagonalt höger
+                } else if (rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j + 1) == 'M' && rows[i + 2].charAt(j + 2) == 'A' && rows[i + 3].charAt(j + 3) == 'S') {
+                    times++;
+                    // Diagonalt höger bakvänt
+                } else if (rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j + 1) == 'A' && rows[i + 2].charAt(j + 2) == 'M' && rows[i + 3].charAt(j + 3) == 'X') {
+                    times++;
+                    // Diagonalt vänster
+                } else if (rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j - 1) == 'M' && rows[i + 2].charAt(j - 2) == 'A' && rows[i - 3].charAt(j + 3) == 'S') {
+                    times++;
+                    // Diagonalt vänster bakvänt
+                } else if (rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j - 1) == 'A' && rows[i + 2].charAt(j - 2) == 'M' && rows[i - 3].charAt(j + 3) == 'X') {
+                    times++;
+                }
             }
         }
 
