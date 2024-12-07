59abe8f92ee8e722ee466e7391945e61049904ee
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-12-07 14:16:54 +0100

    Day 4 part 1 2024 more improvements

--- src/Aoc2024/day4.java
+++ src/Aoc2024/day4.java
@@ -26,16 +26,16 @@ public class day4 {
                 } else if (i <= rows.length - 3 && rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j) == 'A' && rows[i + 2].charAt(j) == 'M' && rows[i+3].charAt(j) == 'X') {
                     times++;
                     // Diagonalt höger
-                } else if (j <= rows[i].length() - 3 && i <= rows.length - 3 && rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j + 1) == 'M' && rows[i + 2].charAt(j + 2) == 'A' && rows[i + 3].charAt(j + 3) == 'S') {
+                } else if (j <= rows[i].length() - 3 && i < rows.length - 3 && rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j + 1) == 'M' && rows[i + 2].charAt(j + 2) == 'A' && rows[i + 3].charAt(j + 3) == 'S') {
                     times++;
                     // Diagonalt höger bakvänt
-                } else if (j <= rows[i].length() - 3 && i <= rows.length - 3 && rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j + 1) == 'A' && rows[i + 2].charAt(j + 2) == 'M' && rows[i + 3].charAt(j + 3) == 'X') {
+                } else if (j <= rows[i].length() - 3 && i < rows.length - 3 && rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j + 1) == 'A' && rows[i + 2].charAt(j + 2) == 'M' && rows[i + 3].charAt(j + 3) == 'X') {
                     times++;
                     // Diagonalt vänster
-                } else if (j > 3 && i > 3 && rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j - 1) == 'M' && rows[i + 2].charAt(j - 2) == 'A' && rows[i - 3].charAt(j + 3) == 'S') {
+                } else if (j > 3 && i <= rows.length - 3 && rows[i].charAt(j) == 'X' && rows[i + 1].charAt(j - 1) == 'M' && rows[i + 2].charAt(j - 2) == 'A' && rows[i + 3].charAt(j - 3) == 'S') {
                     times++;
                     // Diagonalt vänster bakvänt
-                } else if (j > 3 && i > 3 && rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j - 1) == 'A' && rows[i + 2].charAt(j - 2) == 'M' && rows[i - 3].charAt(j + 3) == 'X') {
+                } else if (j > 3 && i <= rows.length - 3 && rows[i].charAt(j) == 'S' && rows[i + 1].charAt(j - 1) == 'A' && rows[i + 2].charAt(j - 2) == 'M' && rows[i + 3].charAt(j - 3) == 'X') {
                     times++;
                 }
             }
