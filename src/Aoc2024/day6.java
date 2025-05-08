e3babc755514aa7506a2f4731c7e0be1ffd10443
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2025-05-08 14:00:13 +0200

    [Day 6 part 1 2024] Added check for dupe visitedCoords

--- src/Aoc2024/day6.java
+++ src/Aoc2024/day6.java
@@ -14,27 +14,42 @@ public class day6 {
             Point guard = new Point(0, 0, 0, 0);
             ArrayList<Point> visitedCoords = new ArrayList<>();
             Point nextCoord;
+            boolean neverVisited;
 
             for (int i = 0; i < board.length; i++) {
                 if (board[i].indexOf('^') != -1) {
-                    guard = new Point(board[i].indexOf('^'), i, -1, 0);
+                    guard = new Point(board[i].indexOf('^'), i, 0, -1);
                     break;
                 }
             }
 
+            visitedCoords.add(new Point(guard.getX(), guard.getY(), 0, 0));
+
             while (true) {
                 nextCoord = guard.getNextCoord();
                 if (nextCoord.getX() < 0 || nextCoord.getY() < 0 || nextCoord.getX() > board[0].length() || nextCoord.getY() > board.length) {
+                    System.out.println("Guard has left the area at coords " + guard.getX() + "," + guard.getY());
+                    System.out.println(nextCoord.getX() + "," + nextCoord.getY());
                     break;
                 } else if (board[nextCoord.getY()].charAt(nextCoord.getX()) == '#') {
                     guard.changeDirection();
                 } else {
                     guard.move();
-                    visitedCoords.add(new Point(guard.getX(), guard.getY(), 0, 0));
+
+                    neverVisited = true;
+                    for (Point p : visitedCoords) {
+                        if (p.getX() == guard.getX() && p.getY() == guard.getY()) {
+                            neverVisited = false;
+                            break;
+                        }
+                    }
+                    if (neverVisited) {
+                        visitedCoords.add(new Point(guard.getX(), guard.getY(), 0, 0));
+                    }
                 }
             }
 
-            System.out.println("Guard visited " + visitedCoords + " coordinates");
+            System.out.println("Guard visited " + visitedCoords.size() + " coordinates");
 
             System.out.println("\nCode took " + (System.currentTimeMillis() - time) + " ms to run");
         } catch (Exception e) {
