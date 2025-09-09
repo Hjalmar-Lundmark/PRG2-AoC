6ff88f8c54c37ed388c5435d6a69bc0e414f1c25
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2025-09-09 15:08:11 +0200

    [Day 8 part 2 2024] Added setters for X & Y in Point

--- src/Aoc2024/Point.java
+++ src/Aoc2024/Point.java
@@ -30,6 +30,14 @@ public class Point {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public int getIdentifier() {
         return id;
     }
