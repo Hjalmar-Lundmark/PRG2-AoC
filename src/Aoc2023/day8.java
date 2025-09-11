aa0a6b251c885173855d0ca58b81f7e44cf9e6ae
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2025-09-11 10:33:45 +0200

    [Day 8 part 1 2023] Finds start position

--- src/Aoc2023/day8.java
+++ src/Aoc2023/day8.java
@@ -1,6 +1,7 @@
 package Aoc2023;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class day8 {
     public static void main(String[] args) {
@@ -11,13 +12,20 @@ public class day8 {
             String[] positions = in.split("start\n")[1].split("\n");
             int steps = 0;
             ArrayList<Node> nodes = new ArrayList<Node>();
+            Node start = new Node("","","");
 
             for (String p : positions) {
-                System.out.println(p);
                 nodes.add(new Node(p.split(" = ")[0], p.split("\\(")[1].split(",")[0], p.split(",")[1].split("\\)")[0]));
             }
             System.out.println(nodes.get(0).toString());
 
+            for (Node n : nodes) {
+                if (Objects.equals(n.getPosition(), "AAA")) {
+                    start = n;
+                }
+            }
+
+            System.out.println(start.toString());
 
             while (true) {
 
