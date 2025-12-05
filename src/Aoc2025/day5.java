932d4cd8be6d97ff079e1af83b418d3e086edb0c
Hjalmar Lundmark <lundmarkhjalmar@gmail.com>
2025-12-05 12:47:48 +0100

    [Day 5 part 1 2025] Converting all ranges into id ints

--- src/Aoc2025/day5.java
+++ src/Aoc2025/day5.java
@@ -1,5 +1,6 @@
 package Aoc2025;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class day5 {
@@ -9,8 +10,25 @@ public class day5 {
             String in = globalFuncs.readFile("input/Aoc2025/day5.txt");
             String[] idRanges = in.split("start")[0].split("\n");
             String[] ids = in.split("start\n")[1].split("\n");
+            int result = 0;
+            ArrayList<Integer> validIds = new ArrayList<>();
+
+
             System.out.println(Arrays.toString(idRanges));
             System.out.println(Arrays.toString(ids));
+
+            for (String range : idRanges) {
+                int startNr = Integer.parseInt(range.split("-")[0]);
+                int endNr = Integer.parseInt(range.split("-")[1]);
+
+                for (int i = startNr; i < endNr+1; i++) {
+                    if (!validIds.contains(i)) {
+                        validIds.add(i);
+                    }
+                }
+            }
+
+            System.out.println(validIds);
 
 
             System.out.println("\nCode took " + (System.currentTimeMillis() - time) + " ms to run");
